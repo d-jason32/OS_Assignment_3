1d14363bf61ee02179bc1753d9912f0011b13dbf
Jason Devaraj <jdevaraj01@gmail.com>
2025-02-17 15:10:59 -0500

    Edited comments.

--- app.cpp
+++ app.cpp
@@ -1,11 +1,3 @@
-/*
-Write a C++ program that creates an array of 20 elements. #
-Fill the array elements by using the random number generator. #
-Create a child process and find the minimum number in the second half of the array. 
-Parent process will find the minimum number in the first half of the array. 
-You must use the pipe to pass the minimum of the child to the parent process. 
-Display the minimum numbers found in each process and their processor ids and finally the minimum number of the array.*/
-
 #include <iostream> 
 #include <string.h>
 #include <cstdlib>
